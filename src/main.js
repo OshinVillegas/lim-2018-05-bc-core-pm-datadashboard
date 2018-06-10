4d654338ddf5e7d457a256026c8bd00ab6e7f904
unknown <natalypc27@gmail.com>
2018-06-09 22:49:03 -0500

    subiendo cambios en index, main, style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 const btnLima = document.getElementById("cLima");
+const btndashB = document.getElementById("dashB");
 
 btnLima.addEventListener("click",()=>{
     
         document.getElementById('contenido').style.display="none";
         document.getElementById('contenidoData').style.display="block";
+})
+btndashB.addEventListener("click",()=>{
+    document.getElementById('contenidoData').style.display="none";
+    document.getElementById('contenido').style.display="block";
+   
 })
