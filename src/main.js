52e26e905e315a21e4ddb0261ec661ac57106f53
unknown <natalypc27@gmail.com>
2018-06-09 22:30:45 -0500

    subiendo cambios y agregando imagenes

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const btnLima = document.getElementById("cLima");
+
+btnLima.addEventListener("click",()=>{
+    
+        document.getElementById('contenido').style.display="none";
+        document.getElementById('contenidoData').style.display="block";
+})
