d4acb1334f44f30776916fd4b63952aea66e6063
Oshin Villegas <villegasoshin25@gmail.com>
2018-06-11 13:45:34 -0500

    datos

--- src/main.js
+++ src/main.js
@@ -18,3 +18,44 @@ btnfiltroStudent.addEventListener("click",()=>{
     document.getElementById('contenidoFiltros').style.display="block";
    
 })
+
+let cohorts = null;
+
+var request = new XMLHttpRequest();
+request.open('GET', '../data/cohorts.json', true);
+
+request.onload = function () {  
+    if (request.status == 200) {
+        cohorts = JSON.parse(request.responseText);
+        console.log(cohorts);
+    }
+};
+
+request.send();
+
+let users = null;
+
+request.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json', true);
+
+request.onload = function () {
+    if (request.status == 200) {
+        users = JSON.parse(request.responseText);
+        console.log(users);
+    }
+};
+
+request.send();
+
+let progress = null;
+
+request.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/progress.json', true);
+
+request.onload = function () {
+    if (request.status == 200) {
+        progress = JSON.parse(request.responseText);
+        console.log(progress);
+    }
+};
+
+request.send();
+
