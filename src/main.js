1908b0d96da615947d7addaa1350dd650aeaedf5
NatalyPorras <natalypc27@gmail.com>
2018-06-14 15:32:13 -0500

    subienco caambios

--- src/main.js
+++ src/main.js
@@ -4,35 +4,55 @@ const btnfiltroStudent = document.getElementById("filtroStudent");
 const btnfiltroQuizzes = document.getElementById("filtroQuizzes");
 const dataStudents= document.getElementById("listStudents");
 const dataQuizzes = document.getElementById("listQuizzes");
-const cohortsSelect = document.getElementById("transporte");
+const cohortsSelect = document.getElementById("trainingCenters");
 
+function cargarDatosCohorts(){
+    let request = new XMLHttpRequest(); 
+    request.open('GET', '../data/cohorts.json', true);
+    request.onload = function () {
+      if(request.status === 200){
+       let sedes = JSON.parse(request.response);
+       cargarDatosSelect(sedes);
+
+      }
+    }
+      request.send();
+}
 btnLima.addEventListener("click",()=>{
-        document.getElementById('contenido').style.display="none";
-        document.getElementById('seleccionCohorts').style.display="block";
-        let request = new XMLHttpRequest(); 
-        request.open('GET', '../data/cohorts.json', true);
-        request.onload = function () {
-          if(request.status === 200){
-           let sedes = JSON.parse(request.response);
-        cargarDatosSelect(sedes);
-        } 
-        }
-        request.send();
-})
-function cargarDatosSelect(array) {
+        // document.getElementById('contenidoData').style.display="block";
+        cargarDatosCohorts(event);
 
+        document.getElementById('contenido').style.display="none";
+        document.getElementById('selectCenters').style.display="block";
+})
+ cohortsSelect.addEventListener("change",()=>{
+    document.getElementById('selectCenters').style.display="none";
+            document.getElementById('contenidoData').style.display="block";
+
+  }); 
+function cargarDatosSelect(array) {
     array.sort();
     array.forEach((elemento) => {
+        if(elemento["id"].substring(0,3)=== "lim"){
         cohortsSelect.innerHTML += "<option value='"+elemento["id"]+"'>"+elemento["id"]+"</option>";
-    });
+        }
+    }); 
+}
+function cargarSelect(array) {
+    array.forEach((elemento) => {
+        if(elemento["id"] === "lim-2018-03-pre-core-pw"){
+            document.getElementById('selectCenters').style.display="none";
+            document.getElementById('contenidoData').style.display="block";
+        }
+        }); 
 }
 
-// btndashB.addEventListener("click",()=>{
-//     document.getElementById('contenidoData').style.display="none";
-//     document.getElementById('contenidoFiltros').style.display="none";
-//     document.getElementById('contenido').style.display="block";
+btndashB.addEventListener("click",()=>{
+    document.getElementById('contenidoData').style.display="none";
+    document.getElementById('selectCenters').style.display="none";
+    document.getElementById('contenido').style.display="block";
    
-// })
+})
 // btnfiltroStudent.addEventListener('click',()=>{
    
 //     let request = new XMLHttpRequest(); 
@@ -48,24 +68,24 @@ function cargarDatosSelect(array) {
 // document.getElementById('contenidoData').style.display="none";
 // document.getElementById('contenidoFiltros').style.display="block";
 // });
-// btnfiltroQuizzes.addEventListener('click',()=>{
-//     let request1 = new XMLHttpRequest(); 
-//     request1.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/progress.json', true);
-//     request1.onload = function () {
-//       if(request1.status === 200){
-//        let progress = JSON.parse(request1.response);
-//        //console.log(progress);       console.log(objeto1);
-//        let arry1=Object.keys(progress);
-//        //console.log(arry1);
+// // btnfiltroQuizzes.addEventListener('click',()=>{
+// //     let request1 = new XMLHttpRequest(); 
+// //     request1.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/progress.json', true);
+// //     request1.onload = function () {
+// //       if(request1.status === 200){
+// //        let progress = JSON.parse(request1.response);
+// //        //console.log(progress);       console.log(objeto1);
+// //        let arry1=Object.keys(progress);
+// //        //console.log(arry1);
  
-//         cargandodataQuizzes(arry1);
-//       } 
-//     }
-//     request1.send();
-// document.getElementById('contenidoData').style.display="none";
-// document.getElementById('contenidoFiltros').style.display="block";    
-// })
-// //Secciones de Funciones
+// //         cargandodataQuizzes(arry1);
+// //       } 
+// //     }
+// //     request1.send();
+// // document.getElementById('contenidoData').style.display="none";
+// // document.getElementById('contenidoFiltros').style.display="block";    
+// // })
+// // //Secciones de Funciones
 
 // function cargandodata(jsonObj){
 
@@ -91,35 +111,35 @@ function cargarDatosSelect(array) {
 //     });
     
 // }
-// // function cargandodataQuizzes(jsonObj1){
+// // // function cargandodataQuizzes(jsonObj1){
   
-// //     console.log(jsonObj1);
-// //     for(var i = 0; i < jsonObj1.length; i++){
-// //         let objetito=jsonObj1[]
-// //         console.log(jsonObj1[i]);
-// //     }
-// //    /* for(let i= 0;i<jsonObj1.l)
-// //     console.log(jsonObj1[2]); */
+// // //     console.log(jsonObj1);
+// // //     for(var i = 0; i < jsonObj1.length; i++){
+// // //         let objetito=jsonObj1[]
+// // //         console.log(jsonObj1[i]);
+// // //     }
+// // //    /* for(let i= 0;i<jsonObj1.l)
+// // //     console.log(jsonObj1[2]); */
     
-// //     // jsonObj1.forEach((elemento) => {
+// // //     // jsonObj1.forEach((elemento) => {
         
-// //     //     //se crea una lista de nombres
-// //     // let linewUp= document.createElement("ul");
-// //     // //se le asigna una clase a la etiqueta ul
-// //     // linewUp.className = "listEstudiantes";
-// //     // let linew= document.createElement("li");
-// //     // dataQuizzes.appendChild(linewUp);
-// //     // //se le asigna una clase a la etiqueta li
-// //     // linew.className = "listasEstudiantes";
+// // //     //     //se crea una lista de nombres
+// // //     // let linewUp= document.createElement("ul");
+// // //     // //se le asigna una clase a la etiqueta ul
+// // //     // linewUp.className = "listEstudiantes";
+// // //     // let linew= document.createElement("li");
+// // //     // dataQuizzes.appendChild(linewUp);
+// // //     // //se le asigna una clase a la etiqueta li
+// // //     // linew.className = "listasEstudiantes";
         
-// //     //     linewUp.appendChild(linew);
-// //     //     let rutaDataNombres=document.createElement("a");
-// //     //     rutaDataNombres.textContent = elemento;
-// //     //     rutaDataNombres.href="about:blank";
-// //     // //se le asigna una clase a la etiqueta a
-// //     // rutaDataNombres.className ="rutasNombres";
-// //     // linew.appendChild(rutaDataNombres);
-// //     // });
+// // //     //     linewUp.appendChild(linew);
+// // //     //     let rutaDataNombres=document.createElement("a");
+// // //     //     rutaDataNombres.textContent = elemento;
+// // //     //     rutaDataNombres.href="about:blank";
+// // //     // //se le asigna una clase a la etiqueta a
+// // //     // rutaDataNombres.className ="rutasNombres";
+// // //     // linew.appendChild(rutaDataNombres);
+// // //     // });
 
 
-// // }
+// // // }
