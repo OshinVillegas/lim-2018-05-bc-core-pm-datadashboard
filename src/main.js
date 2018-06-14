0d20c7ab4320dcd76054c27288b657e55466598b
Oshin Villegas <villegasoshin25@gmail.com>
2018-06-14 14:28:23 -0500

    empezandoFetch

--- src/main.js
+++ src/main.js
@@ -5,42 +5,6 @@ const btnfiltroQuizzes = document.getElementById("filtroQuizzes")
 const dataStudents= document.getElementById("listStudents");
 const dataQuizzes = document.getElementById("listQuizzes");
 
-function cargandodata(jsonObj){
-    console.log(jsonObj);
-    jsonObj.forEach((elemento) => {
-
-        //se crea una lista de nombres
-        let linewUp = document.createElement("ul");
-        let linew = document.createElement("li");
-        dataStudents.appendChild(linewUp);
-        linew.className = "listasEstudiantes";
-
-        linewUp.appendChild(linew);
-        let rutaDataNombres = document.createElement("a");
-        rutaDataNombres.textContent = elemento["name"];
-        rutaDataNombres.href = "about:blank";
-        linew.appendChild(rutaDataNombres);
-
-    })
-}
-function cargandodataQ(jsonObj) {
-    console.log(jsonObj);
-    jsonObj.forEach((elemento) => {
-
-        //se crea una lista de nombres
-        let linewUp = document.createElement("ul");
-        let linew = document.createElement("li");
-        dataQuizzes.appendChild(linewUp);
-        linew.className = "listasEstudiantes";
-
-        linewUp.appendChild(linew);
-        let rutaDataNombres = document.createElement("a");
-        rutaDataNombres.textContent = elemento["name"];
-        rutaDataNombres.href = "about:blank";
-        linew.appendChild(rutaDataNombres);
-
-    })
-}
 btnLima.addEventListener("click",()=>{
         document.getElementById('contenido').style.display="none";
         document.getElementById('contenidoData').style.display="block";
@@ -53,41 +17,12 @@ btndashB.addEventListener("click",()=>{
 })
 
 btnfiltroStudent.addEventListener('click',()=>{
-   
-    let request = new XMLHttpRequest(); 
-    request.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json', true);
-
-    request.onload = function () {
-      if(request.status === 200){
-       let users = JSON.parse(request.response);
-       console.log(users);
-       cargandodata(users);
-      } 
-    }
-    request.send();
 document.getElementById('contenidoData').style.display="none";
 document.getElementById('contenidoFiltros').style.display="block";
 })
 
 
 btnfiltroQuizzes.addEventListener('click',()=>{
-
-    let request3 = new XMLHttpRequest();
-    request3.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/progress.json', true);
-    
-    request3.onload = function () {
-        if (request3.status == 200) {
-            let progress = JSON.parse(request3.responseText)
-
-            //let array1 = progress["04oXrfTxNUbhGtkxNTfw7fhHVgs1"].intro;
-            //console.log(array1)
-            let array1 = Object.keys(progress)
-            console.log(array1)
-        }
-    };
-
-    request3.send();
-    
     
     document.getElementById('contenidoData').style.display = "none";
     document.getElementById('contenidoFiltros').style.display = "block";
