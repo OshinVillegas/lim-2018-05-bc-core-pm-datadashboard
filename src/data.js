646fd19b528d67610b765a50f15ffa1656e47c1d
NatalyPorras <natalypc27@gmail.com>
2018-06-21 18:47:33 -0500

    subiendo cambios

--- src/data.js
+++ src/data.js
@@ -1,31 +1,53 @@
 window.computeUsersStats = (users, progress, courses) => {
-    usersWithStats=users
-    users.stats={
-        stats={
-            percent:
-            exercises={
-                total:sdsd,
-                completed:d,
-                percent:d
-            },
-            reads={
-                total:sdsd,
-                completed:d,
-                percent:d
-            },
-            quizzes={
-                total:sdsd,
-                completed:d,
-                percent:d,
-                scoreSum:a,
-                scoreAvg:a
+
+usersWithStats= [
+        users.forEach((user)=>{
+            const userId = user.id;
+            const userProgress = progress[userId];
+            let durationTotal="";
+            let userProgressPercent = 0;
+            //se determina el promedio por alumnas
+            if (userProgress && userProgress.hasOwnProperty('intro') && userProgress.intro.hasOwnProperty('units')) {
+                const units = userProgress.intro.units;
+                const durationTot= userProgress.intro.totalDuration;
+                const progressTotal = Object.keys(units).reduce((sumProgress, unit) => {
+                    return sumProgress + units[unit].percent
+                }, 0)
+                userProgressPercent = progressTotal /  Object.keys(units).length;
+                durationTotal = durationTot;
+                for (let value of units){
+   
+                }
             }
-        }
-    ]
-    }
+            
+            const stats={
+                percent:userProgressPercent,
+                exercises={
+                    total:0,
+                    completed:0,
+                    percent:0
+                },
+                reads={
+                    total:0,
+                    completed:0,
+                    percent:0
+                },
+                quizzes={
+                    total:sdsd,
+                    completed:d,
+                    percent:d,
+                    scoreSum:a,
+                    scoreAvg:a
+                }
+            }
+  
+        },
+                     users.stats = stats,
+]
     return usersWithStats;
     
 };
+
 window.sortUsers = (users, orderBy, orderDirection) => {
 
 };
--- src/main.js
+++ src/main.js
@@ -14,7 +14,6 @@ btnLima.addEventListener("click",()=>{
         return response.json();
     })
     .then (function (dataCohorts) { 
-        dataCohorts.sort();
         dataCohorts.forEach((elemento) => {
             if(elemento["id"].substring(0,3)=== "lim"){
             cohortsSelect.innerHTML += "<option value='"+elemento["id"]+"'>"+elemento["id"]+"</option>";
@@ -26,13 +25,11 @@ btnLima.addEventListener("click",()=>{
                 document.getElementById('selectCenters').style.display="none";
                 document.getElementById('contenidoData').style.display="block";
                 // cargarDatosProgress();
-                
             }
-            
             }); 
     })
     .catch(error => 
-        console.error('Error:', error));
+        console.error('Error: Nat algo haces mal', error));
     document.getElementById('contenido').style.display="none";
     document.getElementById('selectCenters').style.display="block";
 })
@@ -48,15 +45,16 @@ btndashB.addEventListener("click",()=>{
 
 const getAllData = (cb) => {
     fetch ('../data/cohorts.json')
-        .then (function (response) {
+        .then (function (responseC) {
         fetch ('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
             .then (function (responseU) { 
                 fetch ('../data/cohorts/lim-2018-03-pre-core-pw/progress.json')
                     .then (function (responseP) {
-                        Promise.all([ response.json(), responseU.json(), responseP.json()])
+                        Promise.all([ responseC.json(), responseU.json(), responseP.json()])
                             .then(data => {
+                                // console.log(data);
                                 const [cohorts, users, progress] = data;
-                                cb(cohorts, users, progress)
+                                cb(cohorts, users, progress);
                             })
                     })
             })
@@ -66,33 +64,82 @@ const getAllData = (cb) => {
 btnfiltro.addEventListener('change',(e)=>{
 
     if(e.target.value === "students"){
-        getAllData((cohorts, users, progress) => {
+        getAllData( (cohorts, users, progress) => {
+
+            //computeUserStats(users,progress,cohorts.courses);
+
             let celda = '';
+            celda += '<tr id="cabecera">';
+                celda += '<th> ID </th>';
+                celda += '<th> NAME </th>';
+                celda += '<th> SIGNUPCOHORTS </th>';
+                celda += '<th> EJERCICIO </th>';
+                celda += '<th> LECTURAS </th>';
+                celda += '<th> QUIZZES </th>';
+                celda += '</tr>'
             users.forEach((user)=>{
                 const userId = user.id;
-                const userProgress = progress[userId]
-                let userProgressPercent = 0;
+                const userProgress = progress[userId];
+                // let durationTotal="";
+                // let userProgressPercent = 0;
+                let contadorLectura = 0;
+                let contadorQuizzes = 0;
+                let contadorEjercicios = 0;
+                // se determina el promedio por alumnas
                 if (userProgress && userProgress.hasOwnProperty('intro') && userProgress.intro.hasOwnProperty('units')) {
                     const units = userProgress.intro.units;
+                    const durationTot= userProgress.intro.totalDuration;
                     const progressTotal = Object.keys(units).reduce((sumProgress, unit) => {
                         return sumProgress + units[unit].percent
                     }, 0)
-                    userProgressPercent = progressTotal /  Object.keys(units).length;
-                }
-                celda += '<tr>';
-                celda += '<th> ID </th>';
-                celda += '<th> SINGUPCOHORT </th>';
-                celda += '<th> NAME </th>';
-                celda += '<th> ROLE </th>';
-                celda += '</tr>'
+                    // console.log(userProgress.intro.units);
+                    userProgressPercent =(progressTotal/ Object.keys(units).length) + "%";
+                    durationTotal = durationTot;
+                    // console.log(userProgressPercent)
+
+
+                    for(let value in units){
+                        const parts = units[value].parts;
+                        for (const part in parts) {
+                            // if (object.hasOwnProperty(key)) {
+                            //     const element = object[key];
+                                
+                            // }
+
+                            if (parts[part].type==="read"){
+                                contadorLectura = Object.keys(parts).reduce((sumProgress, unit) => {
+                                    return sumProgress + parts[unit].completed
+                                }, 0)
+                            
+                            }
+                            if (parts[part].type==="practice"){
+                                contadorEjercicios = Object.keys(parts).reduce((sumProgress, unit) => {
+                                    return sumProgress + parts[unit].completed
+                                }, 0)
+                            }
+                            if (parts[part].type==="quiz"){
+                                contadorQuizzes = Object.keys(parts).reduce((sumProgress, unit) => {
+                                    return sumProgress + parts[unit].completed
+                                }, 0)
+                            }
+                        }
+                    }   
+                    console.log(contadorEjercicios);    
+                    console.log(contadorLectura);                    
+                    console.log(contadorQuizzes);                    
                 
-                if(user.signupCohort==="lim-2018-03-pre-core-pw"){
-                    celda += '<tr>';
-                    celda += '<td id= "nombrestabla"><a href="" id="usersEstadisticas">' + user.id + '</a></td>';
                     
+                }
+
+                if(user.signupCohort==="lim-2018-03-pre-core-pw"){
+                    celda += '<tr id="cuerpoData">';
+                    celda += '<td id= "nombrestabla"><a href="" id="usersEstadisticas">' + user.id + '</a></td>';
                     celda += '<td>' + user.name + '</td>';
                     celda += '<td>'+ user.signupCohort +'</td>';
-                    celda += '<td>' + userProgressPercent + '</td>';
+                    celda += '<td>' + contadorEjercicios + '</td>';
+                    celda += '<td>' + contadorLectura + '</td>';
+                    celda += '<td>' + contadorQuizzes + '</td>';
+
                     celda += '</tr>';
                 }
             })
